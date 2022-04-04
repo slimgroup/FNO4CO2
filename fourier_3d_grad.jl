bc3d7735e905ab3034189751ccacf76df30c28b3
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2022-04-04 01:11:24 -0400

    add learned coupled inversion

--- fourier_3d_grad.jl
+++ fourier_3d_grad.jl
@@ -1,5 +1,5 @@
-# author: Ziyi Yin
-# This code is an implementation of fourier neural operators from Zongyi Li's repository
+# author: Ziyi Yin, ziyi.yin@gatech.edu 
+
 using DrWatson
 @quickactivate "FNO"
 import Pkg; Pkg.instantiate()
@@ -26,141 +26,93 @@ Random.seed!(3)
 ntrain = 1000
 ntest = 100
 
+# load the network
 BSON.@load "data/TrainedNet/2phasenet_200.bson" NN w batch_size Loss modes width learning_rate epochs gamma step_size
 
-n = (64,64)
-#d = (15f0,15f0) # dx, dy in m
-d = (1f0/64, 1f0/64)
-
-nt = 51
-#dt = 20f0    # dt in day
-dt = 1f0/nt
-
-perm = matread("data/data/perm.mat")["perm"]
-conc = matread("data/data/conc.mat")["conc"]
-
-s = 4
-
-x_train_ = convert(Array{Float32},perm[1:s:end,1:s:end,1:ntrain])
-x_test_ = convert(Array{Float32},perm[1:s:end,1:s:end,end-ntest+1:end])
-
-y_train_ = convert(Array{Float32},conc[:,1:s:end,1:s:end,1:ntrain])
-y_test_ = convert(Array{Float32},conc[:,1:s:end,1:s:end,end-ntest+1:end])
-
-y_train_ = permutedims(y_train_,[2,3,1,4])
-y_test = permutedims(y_test_,[2,3,1,4])
-
-x_normalizer = UnitGaussianNormalizer(x_train_)
-x_train_ = encode(x_normalizer,x_train_)
-x_test_ = encode(x_normalizer,x_test_)
-
-y_normalizer = UnitGaussianNormalizer(y_train_)
-y_train = encode(y_normalizer,y_train_)
-
-x = reshape(collect(range(d[1],stop=n[1]*d[1],length=n[1])), :, 1)
-z = reshape(collect(range(d[2],stop=n[2]*d[2],length=n[2])), 1, :)
-
-grid = zeros(Float32,n[1],n[2],2)
-grid[:,:,1] = repeat(x',n[2])'
-grid[:,:,2] = repeat(z,n[1])
-
-x_train = zeros(Float32,n[1],n[2],nt,4,ntrain)
-x_test = zeros(Float32,n[1],n[2],nt,4,ntest)
-
-for i = 1:nt
-    x_train[:,:,i,1,:] = deepcopy(x_train_)
-    x_test[:,:,i,1,:] = deepcopy(x_test_)
-    for j = 1:ntrain
-        x_train[:,:,i,2,j] = grid[:,:,1]
-        x_train[:,:,i,3,j] = grid[:,:,2]
-        x_train[:,:,i,4,j] .= (i-1)*dt
-    end
-
-    for k = 1:ntest
-        x_test[:,:,i,2,k] = grid[:,:,1]
-        x_test[:,:,i,3,k] = grid[:,:,2]
-        x_test[:,:,i,4,k] .= (i-1)*dt
-    end
-end
-
-# value, x, y, t
-
-Flux.testmode!(NN, true)
-Flux.testmode!(NN.conv1.bn0)
-Flux.testmode!(NN.conv1.bn1)
-Flux.testmode!(NN.conv1.bn2)
-Flux.testmode!(NN.conv1.bn3)
-
+n = (64,64) # spatial dimension
+d = 1f0./n  # normalized spacing
 nx, ny = n
 dx, dy = d
 
-x_test_1 = x_test[:,:,:,:,1:1]
-y_test_1 = y_test[:,:,:,1:1]
+nt = 51     # num of time steps
+dt = 1f0/nt # normalized time stepping
 
-grad_iterations = 50
+# load training pairs, set normalization for input and output
+x_train, x_test, y_train, y_test, x_normalizer, y_normalizer, grid = loadPairs();
+std_, eps_, mean_ = setMeanStd(x_normalizer)
 
+# set test mode of the network
+Flux.testmode!(NN, true)
+Flux.testmode!(NN.conv1.bn0);
+Flux.testmode!(NN.conv1.bn1);
+Flux.testmode!(NN.conv1.bn2);
+Flux.testmode!(NN.conv1.bn3);
+
+# take a test sample
+x_true = decode(x_test[:,:,1,1,1])  # take a test sample
+
+# observation vintages
 nv = nt
 survey_indices = Int.(round.(range(1, stop=nt, length=nv)))
+yobs = y_test[:,:,survey_indices,1] # ground truth CO2 concentration at these vintages
 
-λ = 0f0 # 2 norm regularization
-
-#x = encode(x_normalizer,20f0*ones(Float32,nx,ny))[:,:,1]
+# initial x and its code (by normalization)
 x = zeros(Float32, nx, ny)
 x_init = decode(x_normalizer,reshape(x,nx,ny,1))[:,:,1]
 
-function f(z)
+# function value
+function f(x)
     println("evaluate f")
-    out = decode(y_normalizer,NN(perm_to_tensor(z,nt,grid,dt)))
-    misfit = 0.5f0 * (nt-1)/(nv-1) * norm(out[survey_indices,:,:]-y_test_1[survey_indices,:,:])^2f0
-    prior = 0.5f0 * λ * norm(z)^2f0
-    loss = misfit + prior
-    @show loss, misfit, prior
-    println("fval is = ", loss)
+    loss = 0.5f0 * norm(S(prj(x))-yobs)^2f0
+    println("fval=", loss)
     return loss
 end
 
-function fg!(fval,g,z)
+# function value and gradient
+function fg!(fval,g,x)
     println("evaluate f and g")
-    p = params(z)
+    p = Flux.params(x)
     @time grads = gradient(p) do
-        out = decode(y_normalizer,NN(perm_to_tensor(z,nt,grid,dt)))
-        misfit = 0.5f0 * (nt-1)/(nv-1) * norm(out[survey_indices,:,:]-y_test_1[survey_indices,:,:])^2f0
-        prior = 0.5f0 * λ * norm(z)^2f0
-        global loss = misfit + prior
-        @show loss, misfit, prior
+        global loss = f(x)
         return loss
     end
-    println("fval is = ", loss)
-    copyto!(g, grads.grads[z])
+    copyto!(g, grads.grads[x])
     return loss
 end
 
+# what to do after every iteration
 function callb(os)
-    z = os.metadata["x"] #get current optimization variable (latent z)
-    fval[curr_iter] = f(z)
-    println("iter: "*string(curr_iter)*" f_val: "*string(fval[curr_iter]))
+    x = os.metadata["x"] #get current optimization variable (latent x)
+    fval[curr_iter] = f(x)
+    println("iter: "*string(curr_iter)*" fval: "*string(fval[curr_iter]))
     flush(stdout)
+    # show the result in every iteration
     axloss.plot(fval[1:curr_iter])
-    ax.imshow(decode(z), vmin=20, vmax=120)
+    ax.imshow(decode(x), vmin=20, vmax=120)
     global curr_iter += 1
 
     #if you want the callback to stop the procedure under some conditions, return true
     return false
 end
 
-std_ = x_normalizer.std_[:,:,1]
-eps_ = x_normalizer.eps_
-mean_ = x_normalizer.mean_[:,:,1]
+# bound constraint
+function prj(x; vmin=10f0, vmax=130f0)
+    y = decode(x)
+    z = max.(min.(y,vmax),vmin)
+    return encode(z)
+end
+
+# set up plots
 curr_iter = 1
 _, ax = subplots(nrows=1, ncols=1, figsize=(20,12))
 _, axloss = subplots(nrows=1, ncols=1, figsize=(20,12))
 
+## LBFGS by fg function
 lbfgs_iters = 50
 fval = zeros(Float32, lbfgs_iters+1)
 res = optimize(only_fg!(fg!), x, LBFGS(), Options(iterations=lbfgs_iters, extended_trace=true, callback=callb))
 
-x_true = decode(x_normalizer,x_test_1[:,:,1:1,1,1])[:,:,1]
-
+## compute true and plot
 figure(figsize=(20,12));
 subplot(1,3,1)
 imshow(x_init,vmin=20,vmax=120);title("initial permeability");
--- utils.jl
+++ utils.jl
@@ -33,3 +33,63 @@ function encode(x::AbstractMatrix{Float32})
     x1 = (x .- mean_) ./ (std_ .+ eps_) 
     return x1
 end
+
+function loadPairs()
+
+# load training pairs
+perm = matread("data/data/perm.mat")["perm"];
+conc = matread("data/data/conc.mat")["conc"];
+
+# spatial downsampling
+s = 4
+
+x_train_ = convert(Array{Float32},perm[1:s:end,1:s:end,1:ntrain])
+x_test_ = convert(Array{Float32},perm[1:s:end,1:s:end,end-ntest+1:end])
+
+y_train_ = convert(Array{Float32},conc[:,1:s:end,1:s:end,1:ntrain])
+y_test_ = convert(Array{Float32},conc[:,1:s:end,1:s:end,end-ntest+1:end])
+
+y_train_ = permutedims(y_train_,[2,3,1,4])
+y_test = permutedims(y_test_,[2,3,1,4])
+
+x_normalizer = UnitGaussianNormalizer(x_train_)
+x_train_ = encode(x_normalizer,x_train_)
+x_test_ = encode(x_normalizer,x_test_)
+
+y_normalizer = UnitGaussianNormalizer(y_train_)
+y_train = encode(y_normalizer,y_train_)
+
+x = reshape(collect(range(d[1],stop=n[1]*d[1],length=n[1])), :, 1)
+z = reshape(collect(range(d[2],stop=n[2]*d[2],length=n[2])), 1, :)
+
+grid = zeros(Float32,n[1],n[2],2)
+grid[:,:,1] = repeat(x',n[2])'
+grid[:,:,2] = repeat(z,n[1])
+
+x_train = zeros(Float32,n[1],n[2],nt,4,ntrain)
+x_test = zeros(Float32,n[1],n[2],nt,4,ntest)
+
+for i = 1:nt
+    x_train[:,:,i,1,:] = deepcopy(x_train_)
+    x_test[:,:,i,1,:] = deepcopy(x_test_)
+    for j = 1:ntrain
+        x_train[:,:,i,2,j] = grid[:,:,1]
+        x_train[:,:,i,3,j] = grid[:,:,2]
+        x_train[:,:,i,4,j] .= (i-1)*dt
+    end
+
+    for k = 1:ntest
+        x_test[:,:,i,2,k] = grid[:,:,1]
+        x_test[:,:,i,3,k] = grid[:,:,2]
+        x_test[:,:,i,4,k] .= (i-1)*dt
+    end
+end
+return x_train, x_test, y_train, y_test, x_normalizer, y_normalizer, grid
+end
+
+function setMeanStd(x_normalizer)
+    std_ = x_normalizer.std_[:,:,1]
+    eps_ = x_normalizer.eps_
+    mean_ = x_normalizer.mean_[:,:,1]
+    return std_, eps_, mean_
+end
