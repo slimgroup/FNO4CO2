6da4b9704870527f634df4df0bd1b6f292be7d51
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-03-10 00:41:20 -0500

    gpu

--- scripts/perl_submit.sh
+++ scripts/perl_submit.sh
@@ -19,6 +19,7 @@ nvidia-smi
 export SLURM_CPU_BIND="cores"
 export PATH=$PATH:$HOME/.julia/bin
 export DFNO_3D_GPU=1
+export FNO4CO2GPU=1
 # export LD_LIBRARY_PATH=
 export LD_PRELOAD=/opt/cray/pe/lib64/libmpi_gtl_cuda.so.0
 module load cudnn/8.9.3_cuda12 julia/1.9
